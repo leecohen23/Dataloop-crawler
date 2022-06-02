3f75afd2ea9cc11ea616b361d32a855d39ae9264
leecohen23 <leecohen23@gmail.com>
2022-06-02 18:29:08 +0300

    Add crawler.js main methods, converted to ESM
    - Converted to ESM in package.json
    - crawler.js: Add getData method and main crawler method

--- crawler.js
+++ crawler.js
@@ -1,31 +1,41 @@
 import fs from 'fs/promises';
+import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
 
 const urlQueue = [];
 const seenAtDepth = {};
-const imgs = {results : []}
+const imgs = { results: [] }
 
 // const cleanUrl = (url) => {
 
 // }
 
-// const getData = () => {
+const getData = async (url) => {
+    const response = await fetch(url);
+    const html = await response.text();
+    // console.group(html);
+    let $ = cheerio.load(html);
+    const links = $('a').map((i, element) => element.attribs.href).get();
+    const images = $('img').map((i, element) => element.attribs.src).get();
+    console.log(links, images);
+    return { links, images };
+}
 
-// }
-
-// const processUrl = () => {
-
-// }
+const processUrl = (url, links, images) => {
+//add img and process children (clean children url)
+}
 
 const crawl = async (startUrl) => {
     seenAtDepth[startUrl] = 0;
     urlQueue.push(startUrl);
-    while (urlQueue.length > 0){
+    while (urlQueue.length > 0) {
         const currUrl = urlQueue.shift();
-        const {links, images} = getData(currUrl);
+        const { links, images } = await getData(currUrl);
         processUrl(url, links, images);
     }
-    await fs.writeFile('./response.txt', imgs)
-    
+    await fs.writeFile('./response.txt', JSON.stringify(imgs))
+
 }
 
-crawl('https://www.fivesigmalabs.com/');
+const urlTest = 'https://www.google.com/';
+crawl(urlTest);
